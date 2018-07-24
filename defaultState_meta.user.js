79594f17f82485317ba3b2b69c042554fb7c195b
Jun Kim <chongjk88@hotmail.com>
2018-07-24 07:26:34 -0700

    Create defaultState_meta.user.js

---
+++ defaultState_meta.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name Salesforce Launch Toolbar
+// @namespace www.cobaltgroup.com
+// @include https://cdk.my.salesforce.com/a1E*
+// @include https://cdk--c.na57.visual.force.com/*
+// @version 4.8.3
+// @run-at document-end
+// @downloadURL https://github.com/cirept/salesforceToolbar/raw/master/launchToolbar/js/launchToolbar_meta.user.js
+// @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+// @resource fontAwe https://use.fontawesome.com/3953f47d82.js
+// @resource googFont https://fonts.googleapis.com/css?family=Raleway:600,900
+// @resource jqueryUI https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
+// @require https://github.com/cirept/salesforceToolbar/raw/master/launchToolbar/js/launchToolbar.js
+// @author cire
+// @grant unsafeWindow
+// @grant GM_setClipboard
+// @grant GM_openInTab
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant window.close
+// @grant window.focus
+// @grant GM_getResourceURL
+// @grant GM_info
+// @grant GM_listValues
+// @noframes
+// ==/UserScript==
+
+
+// ==UserScript==
+// @name         Auto Default State
+// @version      0.2
+// @author       jk
+// @include      http://websites.cobalt.com/wsm/route/base-view/contentEditor*
+// @downloadURL  https://github.com/cirept/salesforceToolbar/raw/master/launchToolbar/js/launchToolbar_meta.user.js
+// @require      https://raw.githubusercontent.com/chongjk88/defaultState/master/defaultState.js
+// @grant        none
+// ==/UserScript==
