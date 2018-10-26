6c7dc4a2463c53dab5d517289aeda4e5f0650eca
Jun Kim <jun.kim@cdk.com>
2018-10-26 13:28:43 -0700

    ssl update

--- defaultState_meta.user.js
+++ defaultState_meta.user.js
@@ -3,6 +3,7 @@
 // @version      0.2
 // @author       jk
 // @include      http://websites.cobalt.com/wsm/route/base-view/contentEditor*
+// @include      https://websites.cobalt.com/wsm/route/base-view/contentEditor*
 // @downloadURL  https://raw.githubusercontent.com/chongjk88/defaultState/master/defaultState_meta.user.js
 // @require      https://raw.githubusercontent.com/chongjk88/defaultState/master/defaultState.js
 // @grant        none
