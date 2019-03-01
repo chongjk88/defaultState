bc597a112bd88043cbc10cde8857f5e9e681cf16
Jun Kim <jun.kim@cdk.com>
2019-03-01 09:35:13 -0800

    Improved URL

--- defaultState_meta.user.js
+++ defaultState_meta.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Auto Default State
-// @version      0.4
+// @version      0.5
 // @author       jk
-// @include      */wsm/route/base-view/contentEditor*
+// @include      *.com/wsm/route/base-view/contentEditor*
 // @downloadURL  https://raw.githubusercontent.com/chongjk88/defaultState/master/defaultState_meta.user.js
 // @require      https://raw.githubusercontent.com/chongjk88/defaultState/master/defaultState.js
 // @grant        none
