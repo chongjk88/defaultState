ba45631e94a4793ab8039c8d8e461abb3e5428d0
Jun Kim <jun.kim@cdk.com>
2019-03-01 09:33:44 -0800

    Update URL

--- defaultState_meta.user.js
+++ defaultState_meta.user.js
@@ -1,9 +1,8 @@
 // ==UserScript==
 // @name         Auto Default State
-// @version      0.3
+// @version      0.4
 // @author       jk
-// @include      http://websites.cobalt.com/wsm/route/base-view/contentEditor*
-// @include      https://websites.cobalt.com/wsm/route/base-view/contentEditor*
+// @include      */wsm/route/base-view/contentEditor*
 // @downloadURL  https://raw.githubusercontent.com/chongjk88/defaultState/master/defaultState_meta.user.js
 // @require      https://raw.githubusercontent.com/chongjk88/defaultState/master/defaultState.js
 // @grant        none
